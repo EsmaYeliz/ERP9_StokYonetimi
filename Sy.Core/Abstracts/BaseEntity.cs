af9618127744b212f9ef9b70426d9607194d7a59
EsmaYeliz <esmayeliz1994@gmail.com>
2019-09-03 10:52:51 +0300

    .

---
+++ Sy.Core/Abstracts/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sy.Core.Abstracts
+{
+    public abstract class BaseEntity<IKey>: IEntity<IKey>
+    {
+        [Key]
+        public IKey Id { get; set; }
+
+    }
+}
